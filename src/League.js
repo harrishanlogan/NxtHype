be9b32843e954d0c820b7cf0500f3d7b22d094df
Harrishan <harrishanlogan@gmail.com>
2021-01-09 14:23:40 -0500

    able to get data for randomized champ

--- src/League.js
+++ src/League.js
@@ -6,17 +6,33 @@ function League() {
     fetchChamps();
   }, []);
 
-  const [items, setItems] = useState([]);
-
   const fetchChamps = async () => {
     const url =
       "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json";
     const data = await fetch(url);
     const items = await data.json();
     var obj = items.data;
-    console.log(
-      Object.keys(obj)[Math.floor(Math.random() * Object.keys(obj).length)]
-    );
+    var champ = Object.keys(obj)[
+      Math.floor(Math.random() * Object.keys(obj).length)
+    ];
+    var i;
+    for (i = 0; i < Object.keys(obj).length; ) {
+      if (JSON.stringify(Object.keys(obj)[i]) === JSON.stringify(champ)) {
+        console.log(Object.values(obj)[i]);
+        break;
+      } else {
+        i++;
+      }
+    }
+    //   if (
+    //     JSON.stringify(Object.keys(obj).length) ===
+    //     JSON.stringify(Object.keys(obj)[i])
+    //   ) {
+    //     console.log(i);
+    //   } else {
+    //     i = i + 1;
+    //   }
+    // }
   };
   return <div>hi</div>;
 }
