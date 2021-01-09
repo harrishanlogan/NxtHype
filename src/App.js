56541617ef9d80fbbacd8f80d70a097d69526c83
Harrishan <harrishanlogan@gmail.com>
2021-01-08 20:02:49 -0500

    Removed links and made it look cleaner with a better purpose of background
    Signed-off-by: Harrishan <harrishanlogan@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import './App.css';
-import Anime from './Anime';
-import League from './League';
-import Nav from './Nav';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-
+import React from "react";
+import "./App.css";
+import Anime from "./Anime";
+import League from "./League";
+import Nav from "./Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div >
-      <Nav />
-      <Route path="/" exact component={Home}/>
-      <Route path="/League" component={League}/>
-      <Route path="/Anime" component={Anime}/>  
-    </div>
+      <div>
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/League" component={League} />
+        <Route path="/Anime" component={Anime} />
+      </div>
     </Router>
   );
 }
 
-const Home = () => (  
+const Home = () => (
   <div className="container">
-    <h1 id="hometitle">WHAT ARE YOU HYPED FOR?</h1>
+    <Link to="/Anime">
+      <h3 className="picstyle">Anime Hype</h3>
+    </Link>
+    <Link to="/League">
+      <h3 className="picstyle">League Hype</h3>
+    </Link>
   </div>
-)
+);
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import './App.css';
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
-    const Navstyle = {
-        color: 'white'
-    }
+  const Navstyle = {
+    color: "white",
+  };
 
-    return (
-        <nav>
-            <h3> NXTHYPE</h3>
-            <ul className="nav-links">
-                <Link style={Navstyle} to="/League">
-                    <li>League Items Ranker</li>
-                </Link>
-                <Link style={Navstyle} to="/Anime">
-                    <li>Upcoming Anime Ranker</li>
-                </Link>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <Link to="/">
+        <h3 className="nxtstyle"> NXTHYPE</h3>
+      </Link>
+      <ul className="nav-links">
+        <Link style={Navstyle} to="/League">
+          <li>League Items Ranker</li>
+        </Link>
+        <Link style={Navstyle} to="/Anime">
+          <li>Upcoming Anime Ranker</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
