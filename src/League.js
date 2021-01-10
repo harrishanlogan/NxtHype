b509efe4fbc81108a85c35993cd5f2149d0a0480
Harrishan <harrishanlogan@gmail.com>
2021-01-10 18:35:39 -0500

    centered image

--- src/League.js
+++ src/League.js
@@ -6,35 +6,42 @@ function League() {
     fetchChamps();
   }, []);
 
+  const [Icon, setIcon] = useState(null);
+
   const fetchChamps = async () => {
     const url =
-      "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json";
+      "http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json";
     const data = await fetch(url);
     const items = await data.json();
     var obj = items.data;
     var champ = Object.keys(obj)[
       Math.floor(Math.random() * Object.keys(obj).length)
     ];
-    var i;
-    for (i = 0; i < Object.keys(obj).length; ) {
-      if (JSON.stringify(Object.keys(obj)[i]) === JSON.stringify(champ)) {
-        console.log(Object.values(obj)[i]);
-        break;
-      } else {
-        i++;
-      }
-    }
-    //   if (
-    //     JSON.stringify(Object.keys(obj).length) ===
-    //     JSON.stringify(Object.keys(obj)[i])
-    //   ) {
-    //     console.log(i);
+    setIcon(champ);
+
+    // var i;
+    // for (i = 0; i < Object.keys(obj).length; ) {
+    //   if (JSON.stringify(Object.keys(obj)[i]) === JSON.stringify(champ)) {
+    //     console.log(Object.values(obj)[i]);
+    //     break;
     //   } else {
-    //     i = i + 1;
+    //     i++;
     //   }
     // }
   };
-  return <div>hi</div>;
+  return (
+    <div>
+      <img
+        src={
+          Icon
+            ? "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" +
+              Icon +
+              "_0.jpg"
+            : ""
+        }
+      />
+    </div>
+  );
 }
 
 export default League;
