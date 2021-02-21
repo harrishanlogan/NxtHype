9b2c7b16d22ec8a39faa5649b52f2d665c301318
Harrishan <harrishanlogan@gmail.com>
2021-02-21 03:44:51 -0500

    edit

--- src/Anime.js
+++ src/Anime.js
@@ -4,11 +4,13 @@ import "./App.css";
 function Anime() {
   return (
     <div>
+      <h1> Currently not functioning </h1>
+      <h3>Check out the League Champ guesser</h3>
       <input
         type="text"
         className="searchcontainer"
         spellcheck="false"
-        placeholder="Enter MAL username"
+        placeholder="Enter MAL username..."
       />
     </div>
   );
